24b89c2f68e2c2a36ffea960c0ed64047fed66a8
nathenqian <zczyqdc@126.com>
2016-03-02 11:19:59 +0800

    fix comma bug

--- labcodes/lab1/kern/init/init.c
+++ labcodes/lab1/kern/init/init.c
@@ -85,9 +85,9 @@ static void
 lab1_switch_to_user(void) {
     //LAB1 CHALLENGE 1 : TODO
     asm volatile(
-        "sub $0x08 %%esp\n"
-        "int %0\n"
-        "add $0x08 %%esp\n"
+        "sub $0x08, %%esp\n;"
+        "int %0\n;"
+        "add $0x08, %%esp;\n"
         : : "i"(T_SWITCH_TOU));
 }
 
@@ -95,9 +95,9 @@ static void
 lab1_switch_to_kernel(void) {
     //LAB1 CHALLENGE 1 :  TODO
     asm volatile(
-        "sub $0x08 %%esp\n"
-        "int %0\n"
-        "add $0x08 %%esp\n"
+        "sub $0x08, %%esp\n;"
+        "int %0\n;"
+        "add $0x08, %%esp\n;"
         : : "i"(T_SWITCH_TOK));
 }
 
