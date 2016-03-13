4ad6708fed5e5be921aafa4971301b59f688913b
nathenqian <zczyqdc@126.com>
2016-03-13 20:31:19 +0800

    add trap int

--- labcodes/lab2/kern/trap/trap.c
+++ labcodes/lab2/kern/trap/trap.c
@@ -159,6 +159,11 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ticks += 1;
+        if (ticks == TICK_NUM) {
+            ticks = 0;
+            print_ticks();
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
