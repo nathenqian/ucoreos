b975a2b0f51e44908167a3ee38e2a820c9f3021e
nathenqian <zczyqdc@126.com>
2016-03-14 11:04:36 +0800

    fix bug

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -99,7 +99,7 @@ default_alloc_pages(size_t n) {
             smaller_page->property = n - p->property;
             prev = list_prev(le);
             list_del(le);
-            list_add_after(prev, smaller_page);
+            list_add_after(prev, &(smaller_page->page_link));
             return p;
         } else 
         if (p->property == n) {
