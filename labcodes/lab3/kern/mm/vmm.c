ff17f957ac5e63d65c7fdc82de9b2aa269caafa7
nathenqian <zczyqdc@126.com>
2016-03-29 11:06:50 +0800

    fix bug

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -415,7 +415,7 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
         if (swap_init_ok) {
             struct Page *page = NULL;
             swap_in(mm, addr, &page);
-            page_insert(mm-p>gdir, page, addr, perm);
+            page_insert(mm->pgdir, page, addr, perm);
             page->pra_vaddr = addr;
             swap_map_swappable(mm, addr, page, 1);
                                     //(1）According to the mm AND addr, try to load the content of right disk page
