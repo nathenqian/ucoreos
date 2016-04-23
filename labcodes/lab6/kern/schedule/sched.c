098cdaa9995acfffb0fff52a8fe3d36491259c67
nathenqian <zczyqdc@126.com>
2016-04-23 16:47:46 +0800

    fix bug

--- labcodes/lab6/kern/schedule/sched.c
+++ labcodes/lab6/kern/schedule/sched.c
@@ -47,7 +47,7 @@ sched_init(void) {
     list_init(&timer_list);
 
     // sched_class = &default_sched_class;
-    sched_class = &linux26_sced_class;
+    sched_class = &linux26_sched_class;
 
     rq = &__rq;
     rq->max_time_slice = MAX_TIME_SLICE;
