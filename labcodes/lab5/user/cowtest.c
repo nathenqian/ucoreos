9badf9a7862323f6cdb1a62a8dea3dd364cbd0a1
nathenqian <zczyqdc@126.com>
2016-04-25 17:12:55 +0800

    add user

--- labcodes/lab5/user/cowtest.c
+++ labcodes/lab5/user/cowtest.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <ulib.h>
-
-int a[4096];
+#define MAX 128
+int a[MAX][MAX];
 
 int
 main(void) {
+    int i, j, k;
     cprintf("cow swap out test begin.\n");
-    while (1);
+    for (k = 0; k < 100000; k ++) {
+	for (j = 0; j < MAX;j ++)
+		for (i = 0; i < MAX;i ++)
+			a[i][j] += 1;
+	cprintf("cow status = %d\n", a[0][0]);
+	}
     // int *a = (int *) malloc(4096);
     return 0;
 }
