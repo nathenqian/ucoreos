7092409815c91e4955d469e8a60dfe1c75d9e35a
nathenqian <zczyqdc@126.com>
2016-05-22 13:55:31 +0800

    try

--- labcodes/lab8/kern/process/proc.c
+++ labcodes/lab8/kern/process/proc.c
@@ -441,32 +441,62 @@ put_files(struct proc_struct *proc) {
  * @tf:          the trapframe info, which will be copied to child process's proc->tf
  */
 
+// static int
+// copy_fs(uint32_t clone_flags, struct proc_struct *proc) {
+//     struct files_struct *fs, *oldfs = current->filesp;
+
+//     /* current is a kernel thread */
+    
+//     if (clone_flags & CLONE_FS) {
+//         fs = oldfs;
+//         goto good_fs;
+//     }
+
+//     int ret = -E_NO_MEM;
+//     fs = files_create();
+//     ret = dup_files(fs, oldfs);
+
+//     if (ret != 0) {
+//         goto bad_cleanup_fs;
+//     }
+
+// good_fs:
+//     files_count_inc(fs);
+//     proc->filesp = fs;
+//     return 0;
+// bad_cleanup_fs:
+//     files_destroy(fs);
+// bad_mm:
+//     return ret;
+// }
+
 static int
 copy_fs(uint32_t clone_flags, struct proc_struct *proc) {
-    struct files_struct *fs, *oldfs = current->filesp;
+    struct files_struct *filesp, *old_filesp = current->filesp;
+    assert(old_filesp != NULL);
 
-    /* current is a kernel thread */
-    
     if (clone_flags & CLONE_FS) {
-        fs = oldfs;
-        goto good_fs;
+        filesp = old_filesp;
+        goto good_files_struct;
     }
 
     int ret = -E_NO_MEM;
-    fs = files_create();
-    ret = dup_files(fs, oldfs);
-
-    if (ret != 0) {
-        goto bad_cleanup_fs;
+    if ((filesp = files_create()) == NULL) {
+        goto bad_files_struct;
     }
 
-good_fs:
-    files_count_inc(fs);
-    proc->filesp = fs;
+    if ((ret = dup_files(filesp, old_filesp)) != 0) {
+        goto bad_dup_cleanup_fs;
+    }
+
+good_files_struct:
+    files_count_inc(filesp);
+    proc->filesp = filesp;
     return 0;
-bad_cleanup_fs:
+
+bad_dup_cleanup_fs:
     files_destroy(fs);
-bad_mm:
+bad_files_struct:
     return ret;
 }
 
@@ -489,11 +519,9 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     if (copy_fs(clone_flags, proc) != 0) { //for LAB8
         goto bad_fork_cleanup_kstack;
     }
-    cprintf("before copy_mm current free pages: %d\n", nr_free_pages());
     if (copy_mm(clone_flags, proc) != 0) {
         goto bad_fork_cleanup_kstack;
     }
-    cprintf("after copy_mm current free pages: %d\n", nr_free_pages());
     copy_thread(proc, stack, tf);
 
     bool intr_flag;
